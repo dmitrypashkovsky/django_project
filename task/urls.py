7c09c08e97c7fa160678713cb4b61abcce005874
Dmitry <dmr.pashkovsky@gmail.com>
2017-09-21 22:01:55 +0300

    first update of template

--- task/urls.py
+++ task/urls.py
@@ -3,6 +3,6 @@ from task import views
 
 urlpatterns = [
     url(r'add/', views.add, name='add'),
-    url(r'check/', views.add, name='check'),
-    url(r'result/', views.add, name='result'),
+    url(r'check/', views.check, name='add'),
+    url(r'result/', views.result, name='add'),
 ]
--- task/views.py
+++ task/views.py
@@ -4,16 +4,37 @@ from django.shortcuts import render
 # http://127.0.0.1:8000/task/add
 # Представление, которое предназначено для отображения страницы ввода набора данных
 def add(request):
+
+    # Служебные переменные
+    type_page = "home"
+    headline = "Тестовое задание"
+    title = "Добавление данных"
+    path = "http://" + request.get_host() + "/task"
+
     return render(request, 'task/add.html', locals())
 
 
 # http://127.0.0.1:8000/task/check
 # Представление, которое предназначено для обработки введённых данных в БД
 def check(request):
-    return render(request, 'task/check.html', locals())
+
+    # Служебные переменные
+    type_page = "check"
+    headline = "Тестовое задание"
+    title = "Обработка всех наборов данных"
+    path = "http://" + request.get_host() + "/task"
+
+    return render(request, 'task/result.html', locals())
 
 
 # http://127.0.0.1:8000/task/result
 # Представление, которое предназначено для выполнения тестовой функции
 def result(request):
+
+    # Служебные переменные
+    type_page = "result"
+    headline = "Тестовое задание"
+    title = "Выполнение тестируемой функции"
+    path = "http://" + request.get_host() + "/task"
+
     return render(request, 'task/result.html', locals())
